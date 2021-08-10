6ca072c0d368f06144a6f305b7cab2ccd48cf6cd
jas0412 <4jasmeetkaur2000@gmail.com>
2021-08-11 02:53:18 +0530

    Shared Preferences example added

---
+++ app/src/main/java/com/example/guessinggame/CheckPreferences.java
@@ -0,0 +1,26 @@
+package com.example.guessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CheckPreferences extends AppCompatActivity {
+
+    TextView prefTextView;
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_preferences);
+
+        prefTextView = findViewById(R.id.prefTextView);
+        sp = getSharedPreferences("userInfo", MODE_PRIVATE);
+
+        String name = sp.getString("name", "Empty");
+        prefTextView.setText(name);
+
+    }
+}
